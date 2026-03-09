701a908401021218deaa99fa40a606cb220fc224
utkarsh <utkarshp2001@gmail.com>
2026-03-09 22:00:06 +0100

    M: podman_osu to use tmp dir per run

--- scripts/podman_osu.sh
+++ scripts/podman_osu.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=osu-podman
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=1
 #SBATCH --time=01:30:00
 #SBATCH --output=./../results/podman_osu_results_%j.out
 
 CONTAINER_IMAGE="localhost/osu-benchmarks:latest"
 
+TMPDIR="/tmp/podman-mpirun-${SLURM_JOB_ID}"
+mkdir -p "$TMPDIR"
 
 source ./bench_lib.sh
 
@@ -19,60 +19,60 @@ BASE_DIR=$(pwd)/../
 echo "========================================="
 echo "OSU Latency Test"
 echo "========================================="
-    mpirun -np 2 \
-        --mca orte_tmpdir_base /tmp/podman-mpirun \
+    mpirun -np $SLURM_NTASKS \
+        --mca orte_tmpdir_base "$TMPDIR" \
         podman run \
             --rm \
             --env-host \
-            -v /tmp/podman-mpirun:/tmp/podman-mpirun \
+            -v "$TMPDIR:$TMPDIR" \
             --userns=keep-id \
-            --net=host --pid=host --ipc=host \  
-            $CONTAINER_IMAGE \
+            --net=host --pid=host --ipc=host \
+            "$CONTAINER_IMAGE" \
             osu_latency
 
 echo ""
 echo "========================================="
 echo "OSU Bandwidth Test"
 echo "========================================="
-    mpirun -np 2 \
-        --mca orte_tmpdir_base /tmp/podman-mpirun \
+    mpirun -np $SLURM_NTASKS \
+        --mca orte_tmpdir_base "$TMPDIR" \
         podman run \
             --rm \
             --env-host \
-            -v /tmp/podman-mpirun:/tmp/podman-mpirun \
+            -v "$TMPDIR:$TMPDIR" \
             --userns=keep-id \
             --net=host --pid=host --ipc=host \
-            $CONTAINER_IMAGE \
+            "$CONTAINER_IMAGE" \
             osu_bw
 
 echo ""
 echo "========================================="
 echo "OSU Bidirectional Bandwidth Test"
 echo "========================================="
-    mpirun -np 2 \
-        --mca orte_tmpdir_base /tmp/podman-mpirun \
+    mpirun -np $SLURM_NTASKS \
+        --mca orte_tmpdir_base "$TMPDIR" \
         podman run \
             --rm \
             --env-host \
-            -v /tmp/podman-mpirun:/tmp/podman-mpirun \
+            -v "$TMPDIR:$TMPDIR" \
             --userns=keep-id \
             --net=host --pid=host --ipc=host \
-            $CONTAINER_IMAGE \
+            "$CONTAINER_IMAGE" \
             osu_bibw
 
 echo ""
 echo "========================================="
 echo "OSU Allreduce Test"
 echo "========================================="
-    mpirun -np 2 \
-        --mca orte_tmpdir_base /tmp/podman-mpirun \
+    mpirun -np $SLURM_NTASKS \
+        --mca orte_tmpdir_base "$TMPDIR" \
         podman run \
             --rm \
             --env-host \
-            -v /tmp/podman-mpirun:/tmp/podman-mpirun \
+            -v "$TMPDIR:$TMPDIR" \
             --userns=keep-id \
             --net=host --pid=host --ipc=host \
-            $CONTAINER_IMAGE \
+            "$CONTAINER_IMAGE" \
             osu_allreduce
 
 bench_end
