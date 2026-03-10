f8cf52912e6f8588991c1ad7a513b5c5afee2a4d
utkarsh <utkarshp2001@gmail.com>
2026-03-10 14:39:06 +0100

    M: using bind to socket

--- scripts/apptainer_ior_mdtest.sh
+++ scripts/apptainer_ior_mdtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=apptainer_benchmark
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=4
 #SBATCH --time=00:30:00
 #SBATCH --output=./../results/apptainer_ior_benchmark_%j.out
 
@@ -15,7 +15,7 @@ source ./bench_lib.sh
 
 MPIRUN="mpirun \
     --map-by ppr:2:node \
-    --bind-to core"
+    --bind-to socket"
 
 APPTAINER_RUN="apptainer exec \
     --bind $BASE_DIR:$BASE_DIR \
