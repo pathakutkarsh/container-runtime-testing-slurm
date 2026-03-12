5fad2566da9a1189e055d5b16ee089cc790580bc
utkarsh <utkarshp2001@gmail.com>
2026-03-12 11:30:13 +0100

    M: adding metrics to charliecloud osu script

--- scripts/charliecloud_osu.sh
+++ scripts/charliecloud_osu.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 #SBATCH --job-name=charlecloud_osu
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=2
 #SBATCH --time=00:30:00
-#SBATCH --output=/home/cloud/shared_dir/results/charliecloud_osu_benchmark_%j.out
+#SBATCH --output=./../results/charliecloud_osu_benchmark_%j.out
 
-BASE_DIR=/home/cloud/shared_dir
+BASE_DIR=$(pwd)/../
 
-echo "Start Time: $(date)"
+source ./bench_lib.sh
+
+MPIRUN="mpirun \
+    --np 2 \
+    --bind-to core \
+    --mca btl self,tcp"
+
+CHARLIECLOUD_RUN="ch-run $BASE_DIR/charliecloud/osu-benchmarks --"
+
+bench_start charliecloud_osu
 echo "========================================="
 echo "OSU Latency Test (Using Host MPI)"
 echo "========================================="
 
 # Use host MPI (mpirun from system), but run OSU binary inside container
-mpirun -np 2 \
-  --bind-to core \
-  --mca btl self,tcp \
-ch-run /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
+
+$MPIRUN $CHARLIECLOUD_RUN /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
 
 echo ""
 echo "========================================="
 echo "OSU Bandwidth Test"
 echo "========================================="
 
-mpirun -np 2 \
-  --bind-to core \
-  --mca btl self,tcp \
-ch-run /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw
+$MPIRUN $CHARLIECLOUD_RUN /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw
 
 echo ""
 echo "========================================="
 echo "OSU Bidirectional Bandwidth Test"
 echo "========================================="
 
-mpirun -np 2 \
-  --bind-to core \
-  --mca btl self,tcp \
-ch-run /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
+$MPIRUN $CHARLIECLOUD_RUN /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
 
 echo ""
 echo "========================================="
 echo "OSU Allreduce Test"
 echo "========================================="
 
-mpirun -np 2 \
-  --bind-to core \
-  --mca btl self,tcp \
-ch-run /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce
+$MPIRUN $CHARLIECLOUD_RUN /home/cloud/shared_dir/charliecloud/osu-benchmarks -- /usr/local/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce
 
-echo "End Time: $(date)"
+
+bench_end
+
 echo ""
 echo "========================================="
 echo "Benchmark completed successfully."
