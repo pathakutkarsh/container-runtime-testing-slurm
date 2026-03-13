588514a15028d85a5d3fe58881972bc2de8e9a28
utkarsh <utkarshp2001@gmail.com>
2026-03-13 18:22:16 +0100

    M: mounting tmp in existing location

--- scripts/podman_ior_mdtest.sh
+++ scripts/podman_ior_mdtest.sh
@@ -8,12 +8,13 @@
 # Create test directory
 CONTAINER_IMAGE="localhost/ior-benchmark:latest"
 
-TMPDIR="/tmp/podman-mpirun-${SLURM_JOB_ID}"
-mkdir -p "$TMPDIR"
-
 BASE_DIR=$(pwd)/..
-TEST_DIR="/tmp/ior-${SLURM_JOB_ID}"
+
+TEST_DIR="${BASE_DIR}/ior-${SLURM_JOB_ID}"
+TMPDIR="${BASE_DIR}/podman-mpirun-${SLURM_JOB_ID}"
+
 mkdir -p "$TEST_DIR"
+mkdir -p "$TMPDIR"
 
 MPIRUN="mpirun \
     --map-by ppr:4:node \
@@ -25,7 +26,7 @@ PODMAN_RUN="podman run \
             --rm \
             --env-host \
             -v $TMPDIR:$TMPDIR \
-            -v $TEST_DIR:$TEST_DIR \
+            -v $TEST_DIR:/mnt/sharedir \
             --userns=keep-id \
             --net=host --pid=host --ipc=host \
             $CONTAINER_IMAGE"
@@ -40,20 +41,20 @@ echo "========================================="
 echo "IOR Write Test"
 echo "========================================="
 
-$MPIRUN $PODMAN_RUN /opt/ior/bin/ior -k -w -o $TEST_DIR/ior_testfile -t 1m -b 16m -s 16
+$MPIRUN $PODMAN_RUN /opt/ior/bin/ior -k -w -o /mnt/sharedir/ior_testfile -t 1m -b 16m -s 16
 
 echo ""
 echo "========================================="
 echo "IOR Read Test"
 echo "========================================="
-$MPIRUN $PODMAN_RUN /opt/ior/bin/ior -r -o $TEST_DIR/ior_testfile -t 1m -b 16m -s 16
+$MPIRUN $PODMAN_RUN /opt/ior/bin/ior -r -o /mnt/sharedir/ior_testfile -t 1m -b 16m -s 16
 
 
 echo "========================================="
 echo "MDTEST"
 echo "========================================="
 
-$MPIRUN $PODMAN_RUN /opt/ior/bin/mdtest -d $TEST_DIR/mdtest -n 1000 -i 3 -u -L -F
+$MPIRUN $PODMAN_RUN /opt/ior/bin/mdtest -d /mnt/sharedir/mdtest -n 1000 -i 3 -u -L -F
 
 bench_end
 echo ""
